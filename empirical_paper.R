45001cdf7f0ef5e97c51842bff7aa6706067bd48
fanyak <fani.akritidou@gmail.com>
2021-07-10 23:38:36 +0300

    trying staff out

--- empirical_paper.R
+++ empirical_paper.R
@@ -51,28 +51,31 @@ flt5 <-flt5%>%filter(BYS14>=0)
 flt5 <-flt5%>%filter(F3MARRSTATUS>=0)
 #use children F2D07, F3D11
 flt6 <-flt5%>%filter(F3D07>=0 &  F3D11 >= 0) 
-#use parents education
 
+#use parents education????????
 flt6<-flt6%>%filter(BYPARED>0)
 
-#IV1 out of state of residense
+#IV1 out of state of residense ??????????
 flt7<-flt6%>%filter(F2PS1OUT>=0 | F3PS1OUT>=0)
 flt7$outofstate<-flt7$F2PS1OUT + flt7$F3PS1OUT
 flt7<-flt7%>%filter(outofstate>=0)
 flt7$outofstate<-as.integer(flt7$outofstate>0)
 
-#IV2 taken part in counseling
+#IV taken part in counseling
 flt7<-flt7%>%filter(BYS33L>=0)
 
+#IV for GPA years of coursework
+flt7<-flt7%>%filter(F1S16A>=0)
+
 # logincome of respondent, parents income, gpa, sex, race, biological children, adopted chilren
-d<-flt7%>%select(F3ERN2011, dropouted, logincome, BYINCOME, F3TZSTEM2GPA, BYS14, BYRACE, F3MARRSTATUS, F3D07,  F3D11, outofstate,BYS33L, BYPARED)
+d<-flt7%>%select(F3ERN2011, dropouted, logincome, BYINCOME, F3TZSTEM2GPA, BYS14, BYRACE, F3MARRSTATUS, F3D07,  F3D11, BYS33L, outofstate, F1S16A)
 #d<-flt6%>%select(F3ERN2011, dropouted, logincome, BYINCOME, F3TZSTEM2GPA, BYS14, BYRACE, F3MARRSTATUS, F3D07,  F3D11)
 
 # create factor for categorical variable base income
 d$basceIncome.f <- factor(d$BYINCOME)
 is.factor(d$basceIncome.f)
 
-# create factor for categorical education of parents
+# create factor for categorical education of parents????????
 d$parentseducation.f <- factor(d$BYPARED)
 is.factor(d$parentseducation.f)
 
@@ -84,20 +87,19 @@ d$mard_cohab<-as.integer(d$F3MARRSTATUS%in%c(1,2,4,6))
 d$children.f <-factor(d$F3D07+d$F3D11)
 is.factor(d$children.f)
 
+#create factor for categorical of sum of biological and adoptive children
+d$advancedwork.f <-factor(d$F1S16A)
+is.factor(d$advancedwork.f)
+
 #create indicators for race
-d<-d%>%mutate(indian=as.integer(d$BYRACE==1))
+#base should be white - compare with base BYRACE = 7
+# indian is only 1 -> don't incude it?
+#d<-d%>%mutate(indian=as.integer(d$BYRACE==1))
 d<-d%>%mutate(asian=as.integer(d$BYRACE==2))
 d<-d%>%mutate(black=as.integer(d$BYRACE==3))
 d<-d%>%mutate(hispanic=as.integer(d$BYRACE==4 | d$BYRACE==5))
-d<-d%>%mutate(white=as.integer(d$BYRACE==7))
+d<-d%>%mutate(biracial=as.integer(d$BYRACE==6))
 
-
-#example plot the residuals
-# l1<-lm(d$logincome~d$F3TZSTEM2GPA)
-# summary(l1)
-# res <-residuals(l1)
-# plot(d$F3TZSTEM2GPA, res)
-# plot(l1$fitted.values, res, ylim = c(-2,0), xlim=c(9.8,10.4))
 
 #1. test for endogeneity and weak instruments
 # REF: https://bookdown.org/ccolonescu/RPoE4/random-regressors.html
@@ -112,13 +114,80 @@ library(AER)
 #droppedout.logit<-glm(d$dropouted~d$indian+d$asian+d$black+d$hispanic+d$white+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + d$outofstate + d$BYS33L)
 
 # https://stats.idre.ucla.edu/r/dae/probit-regression/
-droppedout.probit<-glm(d$dropouted~d$indian+d$asian+d$black+d$hispanic+d$white+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + d$outofstate + d$BYS33L, family = binomial(link = "probit"))
+droppedout.probit<-glm(d$dropouted~d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + d$F1S16A, family = binomial(link = "probit"))
 summary(droppedout.probit)
 droppedoutHat <- fitted(droppedout.probit)
 
 #use fitted values to calculate the 2nd stage 
 #https://www.stata.com/statalist/archive/2004-09/msg00339.html
-wage.2sls <- ivreg(d$logincome~d$dropouted+d$indian+d$asian+d$black+d$hispanic+d$white+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncom.f | d$indian+d$asian+d$black+d$hispanic+d$white+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + droppedoutHat)
+wage.2sls <- ivreg(d$logincome~d$dropouted+d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f | d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + droppedoutHat)
 summary(wage.2sls, diagnostics = TRUE)
+
+#plot the residuals
+# REF: https://cran.r-project.org/web/packages/olsrr/vignettes/residual_diagnostics.html
+f<-fitted(wage.2sls)
+rs<-residuals(wage.2sls)
+plot(f,rs)
+abline(0, 0)
 #specification tests
 #https://bookdown.org/ccolonescu/RPoE4/random-regressors.html#mjx-eqn-eqwagelm10
+
+
+#is GPA engogenous
+#use outofState as an instrument for GPA
+#holding all other factors constant, outofState affects GPA
+summary(lm(d$F3TZSTEM2GPA~d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f + d$basceIncome.f + d$advancedwork.f + d$outofstate))
+fmgpa<-ivreg(d$logincome~d$F3TZSTEM2GPA + d$dropouted +d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+ d$basceIncome.f | d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+ d$basceIncome.f + d$outofstate + d$BYS33L)
+summary(fmgpa, diagnostics = TRUE)
+
+fa<-fitted(fmgpa)
+rsa<-residuals(fmgpa)
+plot(fa,rsa)
+abline(0, 0)
+
+#***************************************************
+#*
+#*The key identification condition is that after partialling out the endogenous is still correlated with the instrument (p10)
+#1. TEST THE PROBIT  -instrument BYS33L must be correlated
+# https://stats.idre.ucla.edu/r/dae/probit-regression/
+droppedout.probit<-glm(d$dropouted~d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + d$outofstate + d$F1S16A + d$BYS33L, family = binomial(link = "probit"), data=d)
+summary(droppedout.probit)
+with(droppedout.probit, null.deviance - deviance)
+## change in degrees of freedom
+with(droppedout.probit, df.null - df.residual)
+## chi square test p-value
+with(droppedout.probit, pchisq(null.deviance - deviance, df.null - df.residual, lower.tail = FALSE))
+#Robust t test
+coeftest(droppedout.probit,vcov. = vcovHC, type = "HC1")
+
+#2.TEST instrument for GPA (outofstate)
+gpa.lm <-lm(d$F3TZSTEM2GPA~d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+ d$basceIncome.f + d$outofstate + d$F1S16A + d$BYS33L)
+summary(gpa.lm)
+coeftest(gpa.lm, vcov = vcovHC(gpa.lm, type="HC1"))
+
+
+# is gpa endogenous?
+fmgpa<-ivreg(d$logincome~d$F3TZSTEM2GPA+d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+ d$basceIncome.f | d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+ d$basceIncome.f + d$outofstate)
+summary(fmgpa, diagnostics = TRUE)
+
+#use only droppedout as endogenous
+droppedoutHat <- fitted(droppedout.probit)
+
+#use fitted values to calculate the 2nd stage 
+#https://www.stata.com/statalist/archive/2004-09/msg00339.html
+wage.2sls <- ivreg(d$logincome~d$dropouted + d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + d$outofstate + d$F1S16A | d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + d$outofstate + d$F1S16A + droppedoutHat)
+summary(wage.2sls, diagnostics = TRUE)
+
+#https://www.econometrics-with-r.org/ivr.html
+fm <- ivreg(d$logincome~d$dropouted + d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + d$outofstate + d$F1S16A | d$asian+d$black+d$hispanic+d$biracial+d$male+d$mard_cohab+d$children.f+d$F3TZSTEM2GPA + d$basceIncome.f + d$outofstate + d$F1S16A + d$BYS33L)
+summary(fm, diagnostics = TRUE)
+coeftest(fm, vcov. = vcovHC, type = "HC1")
+
+
+ft<-fitted(fm)
+rta<-residuals(fm)
+plot(ft,rta)
+abline(0, 0)
+attach(d)
+#https://www.econometrics-with-r.org/rwabdv.html
+linearHypothesis(droppedout.probit, test="F",  "d$BYS33L = 0",  vcov. = vcovHC(droppedout.probit, type = "HC1"))
